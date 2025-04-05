3c70e0fb43fe7c0fe479c4f85b5ca916e5ce6446
N1k1ta-white <nikitabeliy02@gmail.com>
2025-04-05 17:56:39 +0300

    practice

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -11,10 +11,35 @@ contract Storage {
 
     uint256 number;
     uint256[10] nums;
+
     mapping (uint8 => string) public names;
+    uint8[15] private keys;
+    uint8 count;
+
+    struct Student {
+        string name;
+        uint16 age;
+    }
+
+    modifier isNumAbove10(uint256 _num) {
+        require(_num > 10, "Number must be > 10!!!");
+
+        _;
+    }
+
+    constructor() {
+        number = 10;
+        count = 0;
+    }
+
+    // constructor(uint16 num) { // At Solidity we can't overload constructors
+
+    // }
 
     function storeName(string memory _name, uint8 _num) public {
         names[_num] = _name;
+        keys[count] = _num;
+        count++;
     }
 
     function getName(uint8 _num) external view returns(string memory) {
@@ -22,7 +47,7 @@ contract Storage {
     }
 
     function storeNums() public {
-        for (uint8 i = 0; i<10 ;i++) {
+        for (uint8 i = 0; i < 10 ; i++) {
             nums[i] = i*2;
         }
     }
@@ -31,11 +56,45 @@ contract Storage {
         return nums;
     }
 
-    function store(uint256 num) public {
-        number = num;
+    function getMapSize() public view returns(uint256) {
+        // return names.length; Map hasn't length field
+    }
+
+    function store(uint256 _num) public isNumAbove10(_num) {
+        // require(num > 10, "Number must be > 10!!!");
+        number = _num;
+    }
+
+    function stopExecution() public pure {
+        revert();
     }
 
     function retrieve() public view returns (uint256){
         return number;
+    }
+
+    function truncate() public pure returns (uint8) {
+        uint8 num = 10;
+
+        return num/3;
+    }
+
+    function isStringEq() public pure returns(bool) {
+        string memory str1 = "123";
+        string memory str2 = "ABC";
+
+        // return keccak256(str1) == keccak256(str2); Error. Function wants binary array as a input
+
+        // return str1 == str2; // TypeError
+        return keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2));
+    }
+ 
+    function useCycle(uint256 _limit) public pure {
+        uint256 result = 0;
+        string memory str = "";
+        for (uint i = 0; i < _limit ; i++) {
+            result += i;
+            str = "SKIBIDI TOILET";
+        }
     }
 }
